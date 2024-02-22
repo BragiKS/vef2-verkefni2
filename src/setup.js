026c1d6cd88e40864eba1ccbe2993acc54b87d98
Bragi Kristófer Sturluson <bks9@hi.is>
2024-02-22 03:07:08 +0000

    database complete woohoo

--- src/setup.js
+++ src/setup.js
@@ -1,10 +1,7 @@
 import { readFile } from 'fs/promises';
 import { createSchema, dropSchema, end, query } from './lib/db.js';
 
-
-
 async function main() {
-  
   const drop = await dropSchema();
 
   if (drop) {
@@ -23,6 +20,7 @@ async function main() {
   }
 
   const data = await readFile('./src/sql/insert.sql');
+  console.info(data.toString('utf-8'));
   const insert = await query(data.toString('utf-8'));
 
   if (insert) {
--- src/sql/insert.sql
+++ src/sql/insert.sql
@@ -1,24 +1,24 @@
-INSERT INTO users (name, username, password, admin) VALUES ('Óli admin', 'admin', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii', true);
+INSERT INTO users (username, password) VALUES ('admin', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii');
 
-INSERT INTO teams (name) VALUES ("Boltaliðið",
-  "Dripplararnir",
-  "Skotföstu kempurnar",
-  "Markaskorarnir",
-  "Sigurliðið",
-  "Risaeðlurnar",
-  "Framherjarnir",
-  "Fljótu fæturnir",
-  "Vinningshópurinn",
-  "Ósigrandi skotfólkið",
-  "Óhemjurnar",
-  "Hraðaliðið");
+INSERT INTO teams (name) VALUES ('Boltaliðið');
+INSERT INTO teams (name) VALUES ('Dripplararnir');
+INSERT INTO teams (name) VALUES ('Skotföstu kempurnar');
+INSERT INTO teams (name) VALUES ('Markaskorarnir');
+INSERT INTO teams (name) VALUES ('Sigurliðið');
+INSERT INTO teams (name) VALUES ('Risaeðlurnar');
+INSERT INTO teams (name) VALUES ('Framherjarnir');
+INSERT INTO teams (name) VALUES ('Fljótu fæturnir');
+INSERT INTO teams (name) VALUES ('Vinningshópurinn');
+INSERT INTO teams (name) VALUES ('Ósigrandi skotfólkið');
+INSERT INTO teams (name) VALUES ('Óhemjurnar');
+INSERT INTO teams (name) VALUES ('Hraðaliðið');
 
-INSERT INTO games (home, away, home_score, away_score) VALUES ('Dripplararnir', 'Óhemjurnar', 3, 5);
-INSERT INTO games (home, away, home_score, away_score) VALUES ('Skotföstu kepurnar', 'Vinningshópurinn', 1, 0);
-INSERT INTO games (home, away, home_score, away_score) VALUES ('Fljótu fæturnir', 'Hraðaliðið', 5, 2);
-INSERT INTO games (home, away, home_score, away_score) VALUES ('Framherjarnir', 'Markaskorarnir', 2, 2);
-INSERT INTO games (home, away, home_score, away_score) VALUES ('Risaeðlurnar', 'Óhemjurnar', 1, 4);
-INSERT INTO games (home, away, home_score, away_score) VALUES ('Boltaliðið', 'Sigurliðið', 2, 3);
-INSERT INTO games (home, away, home_score, away_score) VALUES ('Ósigrandi skotfólið', 'Markaskorarnir', 2, 0);
-INSERT INTO games (home, away, home_score, away_score) VALUES ('Boltaliðið', 'Sigurliðið', 1, 3);
-INSERT INTO games (home, away, home_score, away_score) VALUES ('Fljótu fæturnir', 'Óhemjurnar', 3, 5);
+INSERT INTO games (home, away, home_score, away_score) VALUES (2, 11, 3, 5);
+INSERT INTO games (home, away, home_score, away_score) VALUES (3, 9, 1, 0);
+INSERT INTO games (home, away, home_score, away_score) VALUES (8, 12, 5, 2);
+INSERT INTO games (home, away, home_score, away_score) VALUES (7, 4, 2, 2);
+INSERT INTO games (home, away, home_score, away_score) VALUES (6, 11, 1, 4);
+INSERT INTO games (home, away, home_score, away_score) VALUES (1, 5, 2, 3);
+INSERT INTO games (home, away, home_score, away_score) VALUES (10, 4, 2, 0);
+INSERT INTO games (home, away, home_score, away_score) VALUES (1, 5, 1, 3);
+INSERT INTO games (home, away, home_score, away_score) VALUES (8, 11, 3, 5);
